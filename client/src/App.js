58b3404df190556409757783a054848c832d3668
Mac_Developer <araozangel842@gmail.com>
2022-02-22 11:39:55 +0200

    Using Mui for styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { TaskList } from './components/TaskList';
+import { TaskForm } from './components/TaskForm';
+import { Container } from '@mui/material';
+import { NavBar } from './components/NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<BrowserRouter>
+			<NavBar />
+			<Container>
+				<Routes>
+					<Route path="/" element={<TaskList />} />
+					<Route path="/task/new" element={<TaskForm />} />
+					{/* Edit task route*/}
+				</Routes>
+			</Container>
+		</BrowserRouter>
+	);
 }
-
-export default App;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material';
+import React from 'react';
+
+export const NavBar = () => {
+	return (
+		<div>
+			NavBar
+			<Button variant="contained" color="success">
+				Click here
+			</Button>
+		</div>
+	);
+};
