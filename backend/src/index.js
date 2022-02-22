da11efa6ef822e016bfea1100d9ef9c8a5586e8f
Mac_Developer <araozangel842@gmail.com>
2022-02-22 11:18:26 +0200

    Starting with front side

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
 const taskRoutes = require('./routes/tasks.routes');
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
