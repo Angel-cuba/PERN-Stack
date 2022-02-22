aaeb53a2dcbdd1b82e73854e9277093b547f8bb9
Mac_Developer <araozangel842@gmail.com>
2022-02-22 11:54:50 +0200

    Using useNavigate hook

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,13 +1,25 @@
-import { Button } from '@mui/material';
-import React from 'react';
+import { AppBar, Box, Button, Container, Toolbar, Typography } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
 
 export const NavBar = () => {
+	const navigate = useNavigate();
 	return (
-		<div>
-			NavBar
-			<Button variant="contained" color="success">
-				Click here
-			</Button>
-		</div>
+		<Box>
+			<AppBar position="static" color="transparent">
+				<Container>
+					<Toolbar>
+						<Typography sx={{ flexGrow: 1 }} variant="h5">
+							<Link style={{ textDecoration: 'none', color: 'white', fontWeight: 'bold' }} to="/">
+								PERN Stack
+							</Link>
+						</Typography>
+						<Button variant="contained" color="success" onClick={() => navigate('/task/new')}>
+							New task
+						</Button>
+					</Toolbar>
+				</Container>
+			</AppBar>
+			{/*  */}
+		</Box>
 	);
 };
