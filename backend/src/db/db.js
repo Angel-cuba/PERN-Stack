48409f188c0056198bec35367c0076a36540498a
Mac_Developer <araozangel842@gmail.com>
2022-02-22 10:48:32 +0200

    New config file and cleaning db.js code

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,12 +1,12 @@
 const { Pool } = require('pg');
 require('dotenv').config();
-
+const { db } = require('../keytotheserver/config');
 const pool = new Pool({
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	database: process.env.DB_NAME,
+	user: db.user,
+	password: db.password,
+	host: db.host,
+	port: db.port,
+	database: db.database,
 });
 
 module.exports = pool;
